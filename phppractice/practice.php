41c3d381532d6e6aa4799daba80bdddbabc13b7f
MarkTener <59380518+MarkTener@users.noreply.github.com>
2020-04-28 22:11:01 -0500

    Create practice.php

---
+++ phppractice/practice.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+<script>
+
+var day;
+var assignValue = 5657757;
+
+var years = 35;
+var someText = " years old";
+var gameOver = true;
+
+alert (years + someText);
+
+</script>
+</head>
+
+<body>
+<p> highlight file
+<?php
+
+
+
+
+?>
+
+
+
+
+
+
+
+</p>
+
+
+
+
+
+</body>
+
+
+</html>
